c2a189a859ffdc9de1cedfe7775c985b462f7565
Karan-Palan <karanpalan007@gmail.com>
2024-02-09 00:25:21 +0530

    Made the Navbar responsive

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <section className="h-wrapper">
       <div className="flexCenter innerWidth paddings h-container">
@@ -14,15 +22,23 @@ const Header = () => {
           <a href="#value">Our Facilities</a>
           <a href="#get-started">Student Life</a>
           <button className="button">
-            <a
-              href="tel:
-              9772379100"
-            >
-              Contact Us
-            </a>
+            <a href="tel:9772379100">Contact Us</a>
           </button>
         </div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
       </div>
+      {isMenuOpen && (
+        <div className="mobile-menu">
+          <a href="#residencies">Hostels</a>
+          <a href="#value">Our Facilities</a>
+          <a href="#get-started">Student Life</a>
+          <button className="button">
+            <a href="tel:9772379100">Contact Us</a>
+          </button>
+        </div>
+      )}
     </section>
   );
 };
